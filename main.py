7e1bb8803bd14b457152c779ed593e101c38c932
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2025-10-24 16:44:16 -0300

    Beta - 1.1.9 - Starting to make the code more readable

--- main.py
+++ main.py
@@ -18,77 +18,80 @@ def main_menu():
     while (Check == False):
         try:
             option = int(input("\nSelecione uma das opcoes Acima: "))
-
-            if option == 1:
-
-                nome_aluno = str(input("Informe o nome do aluno: "))
-
-                status_aluno = str(input("Este aluno está tendo aulas? (S/N): ")).upper()
-                if status_aluno == "S":
-                    status_aluno = "Ativo"
-                elif status_aluno == "N":
-                    status_aluno = "Deligado"
-                else:
-                    status_aluno = "UNKNOWN"
-
-                aulas_aluno = int(input("Informe quantas aulas o aluno assistiu: "))
-                pagamento_aluno = str(input("Informe a data do pagamento: "))
-                nivel_aluno = str(input("Informe o nível do aluno: "))
-
-                novo_Aluno = sf.cadastrar_aluno(nome_aluno, 
-                                                status_aluno, 
-                                                aulas_aluno,
-                                                pagamento_aluno,
-                                                nivel_aluno,)
-
-            elif option == 2:
-                sf.visualizar_alunos()
-
-            elif option == 3:
-                alterar_por_nome = str(input("Informe o nome do aluno: "))
-                chave = None
-                valor_atualizado = None
-
-                print("\nSelecione o que deseja alterar: \n")
-                print("1) - Nome")
-                print("2) - Status")
-                print("3) - Quantidade de aulas")
-                print("4) - Dia do pagamento")
-                print("5) - Nivel do aluno")
-
-                selecao = int(input("Selecione um valor: "))
-
-                if selecao == 1:
-                    chave = 'Nome'
-                    valor_atualizado = str(input("Insira o novo Nome: "))
-                elif selecao == 2:
-                    chave = 'Status'
-                    valor_atualizado = str(input("Insira o novo Status: "))
-                elif selecao == 3:
-                    chave = 'Aulas'
-                    valor_atualizado = int(input("Informe a quantidade de Aulas: "))
-                elif selecao == 4:
-                    chave = 'Dia do Pagamento'
-                    valor_atualizado = str(input("Insira o novo dia de Pagamento: "))
-                elif selecao == 5:
-                    chave = 'Nivel'
-                    valor_atualizado = str(input("Insira o novo Nivel: "))
-
-                sf.editar_aluno(alterar_por_nome, chave, valor_atualizado)
-
-            elif option == 4:
-                nome_aluno = str(input("Informe o nome do aluno: "))
-                sf.remover_aluno(nome_aluno)
-
-            elif option == 5:
-                print("\nSaindo do programa...")
-                break
-
-            else:
-                print(f"A Opção '{option}' não exite.")
-
-        except:
-            print(f"Este Caractere provavelmente não é inteiro.")
+        except ValueError:
+            print("Este Caractere provavelmente não é inteiro.")
             Check = False
+        except:
+            print(f"Outra coisa deu errada.")
+            Check = False
+
+
+        if option == 1:
+
+            nome_aluno = str(input("Informe o nome do aluno: "))
+
+            status_aluno = str(input("Este aluno está tendo aulas? (S/N): ")).upper()
+            if status_aluno == "S":
+                status_aluno = "Ativo"
+            elif status_aluno == "N":
+                status_aluno = "Deligado"
+            else:
+                status_aluno = "UNKNOWN"
+
+            aulas_aluno = int(input("Informe quantas aulas o aluno assistiu: "))
+            pagamento_aluno = str(input("Informe a data do pagamento: "))
+            nivel_aluno = str(input("Informe o nível do aluno: "))
+
+            novo_Aluno = sf.cadastrar_aluno(nome_aluno, 
+                                            status_aluno, 
+                                            aulas_aluno,
+                                            pagamento_aluno,
+                                            nivel_aluno,)
+
+        elif option == 2:
+            sf.visualizar_alunos()
+
+        elif option == 3:
+            alterar_por_nome = str(input("Informe o nome do aluno: "))
+            chave = None
+            valor_atualizado = None
+
+            print("\nSelecione o que deseja alterar: \n")
+            print("1) - Nome")
+            print("2) - Status")
+            print("3) - Quantidade de aulas")
+            print("4) - Dia do pagamento")
+            print("5) - Nivel do aluno")
+
+            selecao = int(input("Selecione um valor: "))
+
+            if selecao == 1:
+                chave = 'Nome'
+                valor_atualizado = str(input("Insira o novo Nome: "))
+            elif selecao == 2:
+                chave = 'Status'
+                valor_atualizado = str(input("Insira o novo Status: "))
+            elif selecao == 3:
+                chave = 'Aulas'
+                valor_atualizado = int(input("Informe a quantidade de Aulas: "))
+            elif selecao == 4:
+                chave = 'Dia do Pagamento'
+                valor_atualizado = str(input("Insira o novo dia de Pagamento: "))
+            elif selecao == 5:
+                chave = 'Nivel'
+                valor_atualizado = str(input("Insira o novo Nivel: "))
+
+            sf.editar_aluno(alterar_por_nome, chave, valor_atualizado)
+
+        elif option == 4:
+            nome_aluno = str(input("Informe o nome do aluno: "))
+            sf.remover_aluno(nome_aluno)
+
+        elif option == 5:
+            print("\nSaindo do programa...")
+            break
+
+        else:
+            print(f"A Opção '{option}' não exite.")
     
 main_menu()
